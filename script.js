9c533901fd7d47fe951dd026c468ce6811ab24fa
Manujapabodhana <rpmanuja123@gmail.com>
2025-07-01 20:25:16 +0530

    ee

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-let a= {
-    name: 'amila',
-    age : 23,
-    city:'tangalle'
-}
-let b= {
-    name: 'upul',
-    age : 23,
-    city:'tangalle'
-}
-let c= {
-    name: 'kumara',
-    age : 23,
-    city:'tangalle'
+let employee ={
+    firstname : "Nimal",
+    'first name ' : "Mcodes",
+    lastname :"Perera",
+    age: 20,
+    iscitizen : true,
+
+    getmsg : function(p1){
+        alert (this.firstname+ " " + this.lastname+ " is " +this.age+ " years old");
+
+    }
 }
 
-console.table({a,b,c})
+// Call the method correctly
+employee.getmsg();
